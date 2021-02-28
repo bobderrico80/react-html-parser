d5f59d1e51220463d21fb3ca3cc0322637896a77
Bob D'Errico <bobderrico80@gmail.com>
2021-02-28 16:03:29 -0500

    Updates

---
+++ precache-manifest.9146f0c288e6139bf51c67175f7d26de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a31833a6c2df5b1d8970e522faba1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aea7edf583619f43a2b3",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "4050cb9dbdbaf09ecfc1",
+    "url": "./static/js/2.9a0ead0a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.9a0ead0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea7edf583619f43a2b3",
+    "url": "./static/js/main.6ebe1367.chunk.js"
+  },
+  {
+    "revision": "8dd46a7ad51edf0b4795",
+    "url": "./static/js/runtime-main.60d2ae5d.js"
+  }
+]);
